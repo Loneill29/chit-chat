a50680dae83e0f12d89f122e7d44667694be0529
Linda <oneill.linda21@gmail.com>
2018-11-02 09:16:07 -0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+var config = {
+    apiKey: "AIzaSyBRPeLHabN6YKcnuGD9N5RHgWrGg1zGCE0",
+    authDomain: "chit-chat-8b537.firebaseapp.com",
+    databaseURL: "https://chit-chat-8b537.firebaseio.com",
+    projectId: "chit-chat-8b537",
+    storageBucket: "chit-chat-8b537.appspot.com",
+    messagingSenderId: "45251208663"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-          </nav>
-          <h1>Chit-Chat</h1>
-        </header>
-        <main>
-        </main>
+        <RoomList firebase= {firebase} />
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class RoomList extends Component {
+
+constructor(props) {
+    super(props);
+
+    this.state = {
+    rooms: []
+  };
+
+this.roomsRef = this.props.firebase.database().ref('rooms');
+
+}
+
+componentDidMount() {
+      this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+      });
+    }
+
+
+render () {
+  return (
+    <section className="sidebar">
+      <h1>Chit-Chat</h1>
+        <ul className="room-list">
+          {
+            this.state.rooms.map( (room, index) =>
+            <li className="room" key={index}>
+              {room.name}
+            </li>
+              )
+            }
+          </ul>
+    </section>
+    );
+  }
+}
+
+export default RoomList;
