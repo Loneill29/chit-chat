55f81d8ef3943f22362275c11259ea3fbcf3d9bd
Linda <oneill.linda21@gmail.com>
2018-11-02 14:49:06 -0500

    build for heroku deployment

---
+++ build/precache-manifest.45c67672a40766f643e263ac4ff1d1e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81b8da857595dc8b5891",
+    "url": "/static/css/main.9c04e163.chunk.css"
+  },
+  {
+    "revision": "81b8da857595dc8b5891",
+    "url": "/static/js/main.81b8da85.chunk.js"
+  },
+  {
+    "revision": "a2e31e254c34e8b234e9",
+    "url": "/static/js/1.a2e31e25.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f3824cb1f5e434dbee0f2be30814bba",
+    "url": "/index.html"
+  }
+];
